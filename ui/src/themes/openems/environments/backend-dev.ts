acce7b1eb16a87fb68fc626e4c795ca173a826a8
Angel Cabrera <acabrera@me.com>
2023-12-13 12:46:23 -0500

    Edge and Backend IP addresses

--- ui/src/themes/openems/environments/backend-dev.ts
+++ ui/src/themes/openems/environments/backend-dev.ts
@@ -5,7 +5,8 @@ export const environment: Environment = {
     ...theme, ...{
 
         backend: 'OpenEMS Backend',
-        url: "ws://" + location.hostname + ":8082",
+//        url: "ws://" + location.hostname + ":8082",
+        url: "ws://" + "192.168.1.218" + ":8082",
 
         production: false,
         debugMode: true,
--- ui/src/themes/openems/environments/edge-dev.ts
+++ ui/src/themes/openems/environments/edge-dev.ts
@@ -5,7 +5,8 @@ export const environment: Environment = {
     ...theme, ...{
 
         backend: 'OpenEMS Edge',
-        url: "ws://" + location.hostname + ":8085",
+//        url: "ws://" + location.hostname + ":8085",
+        url: "ws://" + "192.168.1.168" + ":8085",
 
         production: false,
         debugMode: true,
